89515ee1a1ddbe0440bcfee1afab6aba93f3d257
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-06 11:58:38 +0300

    Style the main parts

--- client/src/components/DessertsPage.jsx
+++ client/src/components/DessertsPage.jsx
@@ -11,8 +11,10 @@ export default function DessertsPage() {
           <div className="cart-container">
             <h3>Your Cart (0)</h3>
             <div className="cart">
+              <div className="emty-cart">
               <img src="\images\illustration-empty-cart.svg" alt="emty" />
               <p>Your added items will appear here </p>
+              </div>
             </div>
           </div>
         </div>
