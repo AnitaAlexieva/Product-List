3fc63898d309d73f8a62a2ab4e47cd0630b20ba3
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-08 23:38:39 +0300

    Style the add to cart button

--- client/src/components/DessertItem.jsx
+++ client/src/components/DessertItem.jsx
@@ -3,8 +3,10 @@ export default function DessertItem() {
         <div className="dessert">
         <div className="img-dessert">
           <img src="\images\image-waffle-tablet.jpg" alt="dessert" />
+        <div className="button-add-div">
+        <img src="\public\images\icon-add-to-cart.svg" alt="add-to-cart" />
+        <button className="add-cart-btn">Add to Cart</button>
         </div>
-        <button className="add-cart-btn"><img src="\public\images\icon-add-to-cart.svg" alt="add-to-cart" />Add to Cart</button>
         <div className="dessert-description">
           <h4>Waffle</h4>
           <h3>Waffle with Berries</h3>
