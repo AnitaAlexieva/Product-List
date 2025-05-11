b52d414c85a745fd561ce65f915f7f670ac198a4
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-11 22:28:02 +0300

    Change the add-button content if there is added dessert

--- client/src/components/DessertItem.jsx
+++ client/src/components/DessertItem.jsx
@@ -4,27 +4,43 @@ export default function DessertItem() {
   const [isAdded, setIsAdded] = useState(false);
   const [count, setCount] = useState(0);
 
-  const smallCount = () => {
-    if (count > 0) {
+  const smallCount = () =>{
+    if(count > 0){
       setCount(count - 1)
-
-    } else {
+     
+    }else{
       setCount(0);
-
+      
     }
   }
 
+    return(
+        <div className="dessert">
+        <div className="img-dessert-div">
+          <img className="img-dessert" src="\images\image-waffle-tablet.jpg" alt="dessert" />
+
         </div>
-        <div className="button-add-div">
-        <img src="\public\images\icon-add-to-cart.svg" alt="add-to-cart" />
-        <button className="add-cart-btn">Add to Cart</button>
-        </div>
+       
+        {isAdded ? (
+           <div className="button-add-div">
+          <div className="added-btn">
+            <button onClick={smallCount}>-</button>
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+          </div>
+        ):(
+           <div className="button-add-div">
+          <img src="\images\icon-add-to-cart.svg" alt="add-to-cart" />
+          <button className="add-cart-btn" onClick={() => setIsAdded(true)}>Add to Cart</button>
+          </div>
+        )}
+        
         <div className="dessert-description">
           <h4 className="simple-name">Waffle</h4>
           <h3 className="whole-name">Waffle with Berries</h3>
           <p className="price-p">$6.50</p>  
         </div>
       </div>
-    </div>
-  )
+    )
 }
