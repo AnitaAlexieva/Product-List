9e5781d3c98111821f6669f623d9d3158a6e43f9
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-13 15:47:50 +0300

    Create full-cart

--- client/src/components/DessertsPage.jsx
+++ client/src/components/DessertsPage.jsx
@@ -17,9 +17,24 @@ export default function DessertsPage() {
           <div className="cart-container">
             <h3>Your Cart (0)</h3>
             <div className="cart">
-              <div className="emty-cart">
+              {/* <div className="emty-cart">
               <img className="empty-img" src="\images\illustration-empty-cart.svg" alt="emty" />
               <p className="empty-p">Your added items will appear here </p>
+              </div> */}
+            </div>
+            <div className="cart-2">
+              <div className="full-cart">
+                <div className="cart-descr">
+                    <h4>Classic Tiramisu</h4>
+                    <div className="item-quantity">
+                      <p className="quantity">2x</p>
+                      <p className="one-price">@ $5.50</p>
+                      <p className="all-price">$11.00</p>
+                    </div>
+                </div>
+                <div className="close-mark">
+                  <img src="\images\icon-remove-item.svg" alt="close" />
+                </div>
               </div>
             </div>
           </div>
