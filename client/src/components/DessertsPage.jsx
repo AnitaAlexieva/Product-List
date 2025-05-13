19abd4a413102a23c212ea0ac04e1b55047f0ca4
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-05-13 16:45:51 +0300

    Add more content to the full-cart

--- client/src/components/DessertsPage.jsx
+++ client/src/components/DessertsPage.jsx
@@ -16,12 +16,12 @@ export default function DessertsPage() {
           </div>
           <div className="cart-container">
             <h3>Your Cart (0)</h3>
-            <div className="cart">
-              {/* <div className="emty-cart">
+            {/* <div className="cart">
+              <div className="emty-cart">
               <img className="empty-img" src="\images\illustration-empty-cart.svg" alt="emty" />
               <p className="empty-p">Your added items will appear here </p>
-              </div> */}
-            </div>
+              </div>
+            </div> */}
             <div className="cart-2">
               <div className="full-cart">
                 <div className="cart-descr">
@@ -32,10 +32,22 @@ export default function DessertsPage() {
                       <p className="all-price">$11.00</p>
                     </div>
                 </div>
-                <div className="close-mark">
-                  <img src="\images\icon-remove-item.svg" alt="close" />
+                <div className="close-mark-div">
+                  <img className="close-mark" src="\images\icon-remove-item.svg" alt="close" />
                 </div>
+                
               </div>
+              <div className="order-total-div">
+                  <p>Order Total</p>
+                  <h2>$46.50</h2>
+                </div>
+                <div className="natural">
+                  <img src="\images\icon-carbon-neutral.svg" alt="" />
+                  <p>This is a <span>carbon-natural</span> delivery</p>
+                </div>
+                <div className="confirm-div">
+                <button className="confirm-btn">Confirm Order</button>
+                </div>
             </div>
           </div>
         </div>
